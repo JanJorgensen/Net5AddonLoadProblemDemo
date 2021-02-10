2f06ff933e5852f90cb30b182ab54de667532f3f
JanJorgensen <alt@nman.dk>
2021-02-10 09:31:25 +0100

    Initial commit.

---
+++ TheAddon/AddonMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheAddon
+{
+    public class AddonMain : TheCore.IAddon
+    {
+        public object Action()
+        {
+            System.IO.Ports.SerialPort port = new System.IO.Ports.SerialPort();
+            port.BaudRate = 115200;
+            return null;
+        }
+    }
+}
---
+++ TheCore/AddonLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace TheCore
+{
+    public class AddonLoader
+    {
+        public static void LoadTheAddon()
+        {
+            var assembly = Assembly.LoadFrom("TheAddon.dll");
+            var type = assembly.GetExportedTypes().Where(t => t.Name == "AddonMain").FirstOrDefault();
+            object obj = Activator.CreateInstance(type);
+            var addon = obj as IAddon;
+
+            addon.Action();     // This is where the error shows !
+        }
+    }
+}
